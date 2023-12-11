6b495a4b83a3605f599ab6483d80be6b62c3e8da
Naresh <iamnareshkumawat1@gmail.com>
2023-12-11 15:26:49 +0530

    socket

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('sendChatToServer', (message) => {
+        io.emit('sendChatToClient', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
